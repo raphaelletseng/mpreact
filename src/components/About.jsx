6b5a0f31c0627e82ae9db2f2ef5e07341679a404
Raphaëlle Tseng <raphaelletseng@gmail.com>
2021-12-29 05:41:55 -0500

    morning fiddles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ import "./layout.css";
 function About (){
   return (
     <section id = "about">
-      <div class = "container" className="bg-dark">
-        
+      <div class = "container text-center" className="bg-dark">
+        <h4> This is a website where all the filler images are foster kitties! </h4>
       </div>
     </section>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./layout.css";
 import Val from "../assets/val.png";
 import {About, Footer} from "../components";
+import {motion} from 'framer-motion';
 
 function Home() {
   return (
@@ -15,13 +16,32 @@ function Home() {
         </div>
 
         <div class = "col">
-          <img class = "img-fluid rounded mb-4 mb-lg-2 shadow p-3 mb-5 bg-white "
-          src = {Val}
-          alt="landing"/>
+          <motion.div
+          initial={{
+                opacity: 0,
+                scale: 0.8,
+                y: 20,
+                }}
+          animate={{
+                scale: 1,
+                opacity: 1,
+                y: 0,
+          }}
+          transition={{
+                duration: 1,
+                delay: 0.2,
+          }}
+          >
+          <img class = "img-fluid rounded mb-4 shadow-lg p-0"
+            src = {Val}
+            alt="landing"/>
+          </motion.div>
         </div>
       </div>
       </div>
+
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#2b2b2b" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+
       <About/>
     <Footer/>
     </div>
