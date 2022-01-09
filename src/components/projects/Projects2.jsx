876dce6061a07349afb9677d3d4680c3f06eb473
Raphaëlle Tseng <raphaelletseng@gmail.com>
2022-01-09 14:43:44 -0500

    Update Projects #7

--- src/components/projects/Projects2.jsx
+++ src/components/projects/Projects2.jsx
@@ -12,10 +12,10 @@ import Datallite2 from "../../assets/datallite.png";
 import Datallite3 from "../../assets/projects/datallite.jpeg";
 import Pytorch from "../../assets/technologies/pytorch.png";
 
-import Kyc2 from "../../assets/projects/knowyourcompany.png";
+import Kyc2 from "../../assets/projects/kyc.png";
 import McGillAI from "../../assets/projects/mcgillai.jpeg";
 import McGillLearn from "../../assets/projects/mcgillailearn.png";
-
+import FairPrivate from "../../assets/projects/fairlearn-privacy.png";
 
 
 
@@ -28,46 +28,7 @@ function Projects2() {
 
       </div>
       <div class="row">
-        <div class="col">
-          <ScrollAnimation animateIn="flipInY">
-          <div className = "project-card">
-            <h1> datallite </h1>
-            <img src = {Datallite2} />
-            <div className = "project-description">
-              <p>
 
-              2021 <br/>
-
-              Datallite is a CNN and map-based dashboard that utilises existing
-              research done by the Stanford Sustainable Development and AI Lab to try
-              and address the issues of data sparsity in developing regions.
-              It uses publically available worldbank data, GRID3 Nigeria datasets, google maps satellite imagery,
-              night time lights data, to predict development indicators like education expenditure
-              that can better inform policies that impact social inequality.
-
-              This project was built written in Python in Jupyter Notebooks, with PyTorch, and a demo website was built with React.
-
-              It won the Concertation Montreal AI4Good Accelerator Award at the 2021 AI4Good Demo Day.
-              </p>
-            </div>
-            <div className = "footer-container">
-              <div class = "row">
-              <div class = "link-col">
-              <button><a href = "https://github.com/RayanAlim/Datallite">
-                <i class="fab fa-github"></i>
-              </a></button>
-              <button><a href = "www.github.com/raphaelletseng">
-                <i class="fab fa-github"></i>
-              </a></button>
-              </div>
-              <div class = "tech-col">
-                <img src = {Pytorch} className = "tech"/>
-              </div>
-              </div>
-            </div>
-          </div>
-          </ScrollAnimation>
-        </div>
 
 
         <div class="col">
@@ -76,25 +37,30 @@ function Projects2() {
 
             <img src = {Datallite3} className = "graphic"/>
             <div className = "links">
-              <button><a href = "https://github.com/RayanAlim/Datallite">
-                <i class="fab fa-github"></i>
+              <button><a href = "https://github.com/RayanAlim/Datallite" target = "_blank" rel="noreferrer noopener">
+                {/*}<i class="fab fa-github"></i>*/}
+                code
+
               </a></button>
-              <button><a href = "https://raphaelletseng.github.io/datallite-site/#home">
-                <i class="fas fa-external-link-square-alt"></i>
+              <button><a href = "https://raphaelletseng.github.io/datallite-site/#home" target = "_blank" rel="noreferrer noopener">
+                site
               </a></button>
             </div>
             <div className = "project-description">
+              <p className = "sub-p">
+              Datallite | Summer 2021 | <a href = "https://www.ai4goodlab.com/post/2021-ai-prototypes"
+              title = "AI4GoodLab prototypes" target = "_blank" rel="noreferrer noopener">
+              Winner of the AI4Good Lab Montreal Accelerator Award (CMTL) </a>
+              </p>
               <p>
-              Datallite is a CNN and map-based dashboard that utilises existing
-              research done by the Stanford Sustainable Development and AI Lab to try
-              and address the issues of data sparsity in developing regions.
-              It uses publically available worldbank data, GRID3 Nigeria datasets, google maps satellite imagery,
-              night time lights data, to predict development indicators like education expenditure
-              that can better inform policies that impact social inequality.
 
-              This project was built written in Python in Jupyter Notebooks, with PyTorch, and a demo website was built with React.
+              A machine learning project that builds on existing
+              research done by the <a href = "http://sustain.stanford.edu/predicting-poverty"
+               target = "_blank" rel="noreferrer noopener">Stanford Sustainable Development and AI Lab</a>. We try to
+              address the issues of data sparsity in developing regions and better inform
+              policies that impact social inequality by using a CNN to predict development indicators like
+              education expenditure.
 
-              It won the Concertation Montreal AI4Good Accelerator Award at the 2021 AI4Good Demo Day.
               </p>
             </div>
 
@@ -113,28 +79,30 @@ function Projects2() {
         <div class="col">
         <ScrollAnimation animateIn="flipInY">
           <div className = "project-card">
-            <h1> DATALLITE </h1>
-            <img src = {Datallite2}/>
+
+            <img src = {FairPrivate}/>
             <div className = "links">
-              <button><a href = "https://github.com/RayanAlim/Datallite">
-                <i class="fab fa-github"></i>
+              <button><a href = "https://github.com/raphaelletseng/fairlearn-pytorch" target = "_blank" rel="noreferrer noopener">
+                code
               </a></button>
-              <button><a href = "https://raphaelletseng.github.io/datallite-site/#home">
-                page
+              <button><a href = "https://raphaelletseng.github.io/2021/09/29/Investigating-privacy-and-fairness-in-ml.html" target = "_blank" rel="noreferrer noopener">
+                report
               </a></button>
             </div>
             <div className = "project-description">
+            <p className = "sub-p">
+            Privacy & Fairness in Machine Learning | Winter 2021 | <a href = "https://www.cs.mcgill.ca/~jguo/lab.html"
+             target = "_blank" rel="noreferrer noopener">
+            McGill Knowledge and Software Technology Lab (KAST) </a>
+          </p>
               <p>
-              Datallite is a CNN and map-based dashboard that utilises existing
-              research done by the Stanford Sustainable Development and AI Lab to try
-              and address the issues of data sparsity in developing regions.
-              It uses publically available worldbank data, GRID3 Nigeria datasets, google maps satellite imagery,
-              night time lights data, to predict development indicators like education expenditure
-              that can better inform policies that impact social inequality.
+              An investigation into the impact of unfairness mitigation
+              techniques on privacy preserving machine learning by applying
+              bias mitigation algorithms to differentially private data. The
+              trained models were then evaluated against a variety of fairness metrics.
+              This project made use of
+              <a href = "https://fairlearn.org/" target = "_blank" rel="noreferrer noopener"> Microsoft's Fairlearn.</a>
 
-              This project was built written in Python in Jupyter Notebooks, with PyTorch, and a demo website was built with React.
-
-              It won the Concertation Montreal AI4Good Accelerator Award at the 2021 AI4Good Demo Day.
               </p>
             </div>
 
@@ -142,96 +110,92 @@ function Projects2() {
               <ul className = "tech-list">
                 <li>Pytorch</li>
                 <li>Python</li>
-                <li>React</li>
               </ul>
             </div>
           </div>
         </ScrollAnimation>
         </div>
 
-        <div class="col">
-          <ScrollAnimation animateIn="flipInY">
-          <div className = "project-card">
 
-            <img src = {Kyc2} />
-            <div className = "project-description">
-              <p>
-              Datallite is a CNN and map-based dashboard that utilises existing
-              research done by the Stanford Sustainable Development and AI Lab to try
-              and address the issues of data sparsity in developing regions.
-              It uses publically available worldbank data, GRID3 Nigeria datasets, google maps satellite imagery,
-              night time lights data, to predict development indicators like education expenditure
-              that can better inform policies that impact social inequality.
 
-              This project was built written in Python in Jupyter Notebooks, with PyTorch, and a demo website was built with React.
 
-              It won the Concertation Montreal AI4Good Accelerator Award at the 2021 AI4Good Demo Day.
-              </p>
-            </div>
-            <div className = "footer-container">
-              <div class = "row">
-              <div class = "link-col">
-              <button><a href = "https://github.com/RayanAlim/Datallite">
-                <i class="fab fa-github"></i>
-              </a></button>
-              <a href = "www.github.com/raphaelletseng">
-                <i class="fab fa-github"></i>
-              </a>
-              </div>
-              <div class = "tech-col">
-                <img src = {Pytorch} className = "tech"/>
-              </div>
-              </div>
-            </div>
-          </div>
-          </ScrollAnimation>
-        </div>
+
 
       </div>
 {/* ---------------------*/}
 <div class="row">
 
+<div class="col">
+  <ScrollAnimation animateIn="flipInY">
+  <div className = "project-card">
 
-
-  <div class="col">
-    <ScrollAnimation animateIn="flipInY">
-    <div className = "project-card">
-
-      <img src = {McGillAI} className = "graphic"/>
-      <div className = "links">
-        <button><a href = "https://github.com/RayanAlim/Datallite">
-          code
-        </a></button>
-        <button><a href = "https://raphaelletseng.github.io/datallite-site/#home">
-          page
-        </a></button>
-      </div>
-      <div className = "project-description">
-        <p>
-        Datallite is a CNN and map-based dashboard that utilises existing
-        research done by the Stanford Sustainable Development and AI Lab to try
-        and address the issues of data sparsity in developing regions.
-        It uses publically available worldbank data, GRID3 Nigeria datasets, google maps satellite imagery,
-        night time lights data, to predict development indicators like education expenditure
-        that can better inform policies that impact social inequality.
-
-        This project was built written in Python in Jupyter Notebooks, with PyTorch, and a demo website was built with React.
-
-        It won the Concertation Montreal AI4Good Accelerator Award at the 2021 AI4Good Demo Day.
-        </p>
-      </div>
-
-      <div className = "technologies">
-        <ul className = "tech-list">
-          <li>Pytorch</li>
-          <li>Python</li>
-          <li>React</li>
-        </ul>
-      </div>
-
+    <img src = {McGillAI} className = "graphic"/>
+    <div className = "links">
+      <button><a href = "https://github.com/McGillAISociety/mcgillaiwebsite" target = "_blank" rel="noreferrer noopener">
+        code
+      </a></button>
+      <button><a href = "https://www.mcgillai.com/" target = "_blank" rel="noreferrer noopener">
+        site
+      </a></button>
     </div>
-    </ScrollAnimation>
+    <div className = "project-description">
+      <p className = "sub-p">
+      McGillAI Website | Summer 2020 - Summer 2021
+      </p>
+      <p>
+      A website for the McGill AI Society. Built to consolidate club information, event links, and FAQs,
+       provide additional Montreal AI resources, introduce members to the club executive, and encourage
+       students to sign up for the McGillAI 'Introduction to Machine Learning' Bootcamp.
+
+      </p>
+    </div>
+
+    <div className = "technologies">
+      <ul className = "tech-list">
+
+        <li>React</li>
+
+      </ul>
+    </div>
+
   </div>
+  </ScrollAnimation>
+</div>
+
+<div class="col">
+<ScrollAnimation animateIn="flipInY">
+  <div className = "project-card">
+    <img src = {Kyc2}/>
+    <div className = "links">
+      <button><a href = "https://github.com/cclin130/know_your_company" target="_blank" rel="noreferrer noopener">
+        code
+      </a></button>
+    </div>
+    <div className = "project-description">
+      <p className = "sub-p">
+      Know Your Company | Winter 2019 | Winner of Gender Inclusion Hack & Spark! Inclusion in Tech
+      </p>
+      <p>
+      This is a Chrome extension built for people searching to work with
+      companies that value diversity.
+      It displays employee diversity data - currently race and gender - for
+      30 top tech companies.
+      It also uses the Bing News API to
+      show recent news article related to the workplace environment.
+      </p>
+    </div>
+
+    <div className = "technologies">
+      <ul className = "tech-list">
+        <li>JS</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  </div>
+</ScrollAnimation>
+</div>
+
 
   <div class="col">
   <ScrollAnimation animateIn="flipInY">
@@ -239,16 +203,19 @@ function Projects2() {
 
       <img src = {McGillLearn}/>
       <div className = "links">
-        <button><a href = "https://github.com/RayanAlim/Datallite">
-          <i class="fab fa-github"></i>
+        <button><a href = "https://github.com/McGillAISociety/learnathon2020" target = "_blank" rel="noreferrer noopener">
+          code
         </a></button>
-        <button><a href = "https://raphaelletseng.github.io/datallite-site/#home">
-          <i class="fas fa-external-link-square-alt"></i>
+        <button><a href = "https://mcgillailearn.com/" target = "_blank" rel="noreferrer noopener">
+          site
         </a></button>
       </div>
       <div className = "project-description">
+        <p className = "sub-p">
+        Winter 2021
+        </p>
         <p>
-        Built a React web page for the 2021 McGill AI Learnathon,
+        A website built for the 2021 McGill AI Learnathon,
         an undergraduate conference including 3 tracks of research talks by
         acclaimed AI researchers, hands-on workshops, and career fair sponsor
         presentations and panel discussions
@@ -257,8 +224,9 @@ function Projects2() {
 
       <div className = "technologies">
         <ul className = "tech-list">
-          <li>Pytorch</li>
+
           <li>React</li>
+
         </ul>
       </div>
     </div>
